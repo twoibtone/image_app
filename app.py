ccefd443cca90b23bcbe9645839f77d4da738acf
annasena <109264568+twoibtone@users.noreply.github.com>
2023-08-15 00:04:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from rembg import remove
 from PIL import Image
 
+st.subheader('이미지 배경제거 서비스')
+
 uploaded_file = st.file_uploader("이미지를 업로드하세요", type=['png', 'jpg', 'jpeg'])
 
 if uploaded_file is not None:
